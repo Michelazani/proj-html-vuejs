74f8d0242d82b0dcf6b76d72c8f908f461a58040
Michelazani <michi.zani220@gmail.com>
2024-01-08 17:06:07 +0100

    inserimento props header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,50 @@
 <script >
-
+import MenuList from './MenuList.vue';
 export default{
     name: 'AppHeader',
     data() {
         return {
-            
-        }
+        // arrray contenenti oggetti che utilizzerò nell'header
+        menuHeader : [
+                                {
+                                    id:1,
+                                    link: 'Home'
+                                },
+                                {
+                                    id:2,
+                                    link: 'Apple'
+                                },
+                                {
+                                    id:3,
+                                    link:'Microsoft' 
+                                },
+                                {
+                                    id:4,
+                                    link:'Android' 
+                                },
+                                {
+                                    id:5,
+                                    link:'Forums' 
+                                },
+                                {
+                                    id:6,
+                                    link:'Contact us' 
+                                },
+                                {
+                                    id:7,
+                                    link:'Join us' 
+                                },
+                ]
+            }
+    },
+    components:{
+        MenuList
     }
 }
 </script>
 
 <template>
     <header>
-        
         <!-- upper header con logo img e ul -->
         <section class="upper-header">
             <div class="container">
@@ -20,12 +52,15 @@ export default{
                     <img src="../assets/img/logo.png" alt="Avada Forum logo">
                 </div>
                 <ul>
-                    <!-- li con props -->
+                    <!-- ciclo le props per il totale degli oggetti -->
+                    <MenuList v-for="menu in menuHeader"
+                    :linkMenu="menu.link"/>
+                    <i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i>
                 </ul>
             </div>
         </section>
 
-            <!-- middle header con text + btn -->
+        <!-- middle header con text + btn -->
         <section class="middle-header">
             <div class="container">
                 <h1>Tech Forum!</h1>
@@ -53,9 +88,20 @@ export default{
 header{
     section.upper-header{
         background-color: $bluette;
+        div.container{
+            @include flex (row, space-between,center)
+        }
+        ul{
+            @include flex (row, center,center);
+            list-style-type: none;
+            color: white;
+        }
         img{
-            padding: 1rem;
-            width: 13rem;;
+            padding: 1rem 0;
+            width: 12rem;
+        }
+        i.fa-magnifying-glass{
+            font-size: small;
         }
     }
     
---
+++ src/components/MenuList.vue
@@ -0,0 +1,22 @@
+<script >
+
+export default{
+    name: 'MenuElement',
+    // definisco le props che vado ad utilizzare nel MenuList
+    props: [
+        'linkMenu'
+    ]
+}
+</script>
+
+<template>
+    <!-- scrivo la props che ho riportato nel vfor con :(nome props) tra le grafe  -->
+    <li>{{ linkMenu }}</li>
+</template> 
+
+<style lang="scss" scoped>
+li{
+    padding: 0 1rem;
+    font-size: small;
+}
+</style>
