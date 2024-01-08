996c1cb50425fb212b1ba5c10293dbaf63203b34
Michelazani <michi.zani220@gmail.com>
2024-01-08 22:12:24 +0100

    inserimento article main con img e text

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,36 +5,36 @@ export default{
     data() {
         return {
         // arrray contenenti oggetti che utilizzerò nell'header
-        menuHeader : [
-                                {
-                                    id:1,
-                                    link: 'Home'
-                                },
-                                {
-                                    id:2,
-                                    link: 'Apple'
-                                },
-                                {
-                                    id:3,
-                                    link:'Microsoft' 
-                                },
-                                {
-                                    id:4,
-                                    link:'Android' 
-                                },
-                                {
-                                    id:5,
-                                    link:'Forums' 
-                                },
-                                {
-                                    id:6,
-                                    link:'Contact us' 
-                                },
-                                {
-                                    id:7,
-                                    link:'Join us' 
-                                },
-                ]
+        menuHeader: [
+                {
+                    id: 1,
+                    link: 'Home'
+                },
+                {
+                    id: 2,
+                    link: 'Apple'
+                },
+                {
+                    id: 3,
+                    link: 'Microsoft'
+                },
+                {
+                    id: 4,
+                    link: 'Android'
+                },
+                {
+                    id: 5,
+                    link: 'Forums'
+                },
+                {
+                    id: 6,
+                    link: 'Contact us'
+                },
+                {
+                    id: 7,
+                    link: 'Join us'
+                },
+            ]
             }
     },
     components:{
@@ -48,18 +48,14 @@ export default{
         <!-- upper header con logo img e ul -->
         <section class="upper-header">
             <div class="container">
-                <div>
-                    <img src="../assets/img/logo.png" alt="Avada Forum logo">
-                </div>
+                <img src="../assets/img/logo.png" alt="Avada Forum logo">
                 <ul>
                     <!-- ciclo le props per il totale degli oggetti -->
-                    <MenuList v-for="menu in menuHeader"
-                    :linkMenu="menu.link"/>
-                    <i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i>
+                    <MenuList v-for="menu in menuHeader" :linkMenu="menu.link"/>
+                    <i class="fa-solid fa-magnifying-glass-plus"></i>
                 </ul>
             </div>
         </section>
-
         <!-- middle header con text + btn -->
         <section class="middle-header">
             <div class="container">
@@ -68,16 +64,14 @@ export default{
                 <button class="btn btn-primary">get avada now! > </button>
             </div>
         </section>
-
         <!-- lower header con text centrato  -->
         <section class="lower-header">
-            <div class="s-container mx-auto">
+            <div class="s-container ">
                 <h6> avada forum is the place to be</h6>
                 <p>In quis lectus sed leo elementum faucibus in dapibus dictum. Nulla molestie tortor nec lectus venenatis, sed blandit dui finibus. dolor at, bibendum purus. Dones vehicula nec tortor ac finibus. </p>
             </div>
         </section>
-            
-        </header>
+    </header>
 </template> 
 
 <style lang="scss" scoped>
@@ -89,20 +83,22 @@ header{
     section.upper-header{
         background-color: $bluette;
         div.container{
-            @include flex (row, space-between,center)
-        }
-        ul{
-            @include flex (row, center,center);
-            list-style-type: none;
-            color: white;
-        }
-        img{
-            padding: 1rem 0;
-            width: 12rem;
-        }
+            @include flex(row,space-between,center);
+            ul{
+                @include flex(row,center,center);
+    
+                list-style-type: none;
+                color: white;
+            }
+            img{
+                padding: 1rem 0;
+                width: 12rem;
+            }
         i.fa-magnifying-glass{
             font-size: small;
         }
+        }
+
     }
     
     section.middle-header{
@@ -138,9 +134,6 @@ header{
     section.lower-header{
         background-color: $light-grey;
         text-align: center;
-        h6{
-            color: rgb(82, 82, 82);
-        }
         div p{
             color: $dark-grey;
             padding: 1rem 0 4rem 0;
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,7 +13,7 @@ export default{
 <template>
     <main>
 <!-- container per grandezza uguale . 
-    1.text centrato
+    1.text centrato  FATTO
     2. calc/3 + text
     3.background-img 100% con text e btn
     4.ripeto 2
@@ -23,6 +23,60 @@ export default{
     8.ripeto 6
     9.bkg-img con text e btn 
     10. input+btn  -->
+        <section>
+        <div class="s-container">
+            <h6>phasellus eget metus</h6>
+            <h2>All the latest news</h2>
+            <div class="line-bottom"></div>
+        </div>
+        <div class="container insert-img">
+            <article>
+                <img src="../assets/img/post_feat_img_25-320x202.jpg" alt="phone on a sofa">
+                <h5><b>Morbi vitae dui euismod vulputate sollicitudin</b></h5>
+                <p>October 11th, 2015 | <span>2 comments</span></p>
+                <p>Donec finibus sit amet orci eget ultricies. praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</p>
+            </article>
+            <article>
+                <img src="../assets/img/post_feat_img_24-320x202.jpg" alt="phone taking picture">
+                <h5><b>Vivamus pellenteque, felis quis varius</b></h5>
+                <p>October 11th, 2015 | Comments off </p>
+                <p>Donec finibus sit amet orci eget ultricies. praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</p>
+            </article>
+            <article>
+                <img src="../assets/img/post_feat_img_23-320x202.jpg" alt="watch img">
+                <h5><b>Donec ornare pretium eget scelisque justo</b></h5>
+                <p>October 11th, 2015 | Comments off </p>
+                <p>Donec finibus sit amet orci eget ultricies. praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</p>
+            </article>
+        </div>
+        <div class="container featured-article">
+            <h5>featured article</h5>
+            <h3>Cras malesuada et orci eget pharetra</h3>
+            <p>in quis lectus sed leo elementum faucibus in dapibus dictum. Nullamolestie tortor nec lectus venenatis, sed blandit dui, dolor at bibendum sadips ets ipsums dolores ficilis uns leo lectus.</p>
+            <button type="button" class="btn btn-dark">read more ></button>
+        </div>
+        <div class="container insert-img">
+            <article>
+                <img src="../assets/img/post_feat_img_22-320x202.jpg" alt="phone on a sofa">
+                <h5><b>Fusce sollicitudin nunc sed placerat varius</b></h5>
+                <p>October 11th, 2015 | Comments off </p>
+                <p>Donec finibus sit amet orci eget ultricies. praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</p>
+            </article>
+            <article>
+                <img src="../assets/img/post_feat_img_21-320x202.jpg" alt="phone taking picture">
+                <h5><b>Donec facilis sodales leo sit amet laoreet</b></h5>
+                <p>October 11th, 2015 | Comments off </p>
+                <p>Donec finibus sit amet orci eget ultricies. praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</p>
+            </article>
+            <article>
+                <img src="../assets/img/post_feat_img_20-320x202.jpg" alt="watch img">
+                <h5><b>Proin eu purus sed aru aliquet curabir vens</b></h5>
+                <p>October 11th, 2015 | Comments off </p>
+                <p>Donec finibus sit amet orci eget ultricies. praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</p>
+            </article>
+        </div>
+    </section>
+
     </main>
 </template> 
 
@@ -30,5 +84,75 @@ export default{
     @use '../style/general.scss' as *;
     @use '../style/partials/mixins' as *;
     @use '../style/partials/variables' as *;
+
+    main{
+        text-align: center;
+        h2{
+            font-weight: bold;
+        }
+        hr{
+            width: 5rem;
+            color: $light-orange;
+        }
+        div.line-bottom{
+            padding-top: 1.5rem;
+            width: 4rem;
+            position: relative;
+            left: 13rem;
+            border-bottom: 0.1rem solid $light-orange;
+        }
+        div.container.insert-img{
+            width: calc(100% / 3);
+            padding: 2rem 0.5rem 0 0.5rem;
+            @include flex(row,center,stretch); 
+            article{
+                padding: 0 0.5rem;
+            } 
+            h5{
+                font-size: 0.9rem;
+                padding-top: 0.5rem;
+                padding-bottom: 0.3rem;
+            }
+            h5,p{
+                text-align: start;
+            }
+            p{
+                color:rgb(105, 105, 105) ;
+                font-size: 0.8rem;
+                padding-bottom: 0.5rem;
+
+            }
+            span{
+                color: black;
+            }
+        }
+        div.featured-article{
+            margin-top: 2rem;
+            background-image: url('../assets/img/featured_article_1_bg.jpg');
+            height: 450px;
+            background-size: 100%;
+            text-align: start;
+            padding: 0 5rem;
+            h5{
+                padding-top: 13rem;
+                font-size: 0.9rem;
+                text-transform: uppercase;
+            }
+            h3{
+                padding: 1rem 0;
+                font-weight: bold;
+            }
+            h5,h3,p{
+                color: white;
+            }
+            button.btn{
+                border-radius: 0%;
+                text-transform: uppercase;
+                font-weight: bold;
+                font-size: small;
+                margin-top: 1rem;
+            }
+        }
+    }
 
 </style>
